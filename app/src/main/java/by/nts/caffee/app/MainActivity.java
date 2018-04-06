cc09ae2ca87641ae197d2cb829331d95b78ab30e
Yahor Fralou <yahor.fralou@datalex.com>
2018-04-06 12:48:03 +0300

    Removed Kotlin support. Launcher icon changed.

---
+++ app/src/main/java/by/nts/caffee/app/MainActivity.java
@@ -0,0 +1,17 @@
+package by.nts.caffee.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yahor_Fralou on 4/6/2018 12:44 PM.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
