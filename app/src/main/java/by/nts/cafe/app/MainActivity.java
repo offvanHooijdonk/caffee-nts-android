fb9b0f0cee675ca4d9afbc6ead3192729573e3a8
Yahor Fralou <yahor.fralou@datalex.com>
2018-04-06 13:04:13 +0300

    Changed package name. Changed app name, locale 'ru' added.

--- app/src/main/java/by/nts/cafe/app/MainActivity.java
+++ app/src/main/java/by/nts/cafe/app/MainActivity.java
@@ -1,12 +1,9 @@
-package by.nts.caffee.app;
+package by.nts.cafe.app;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Yahor_Fralou on 4/6/2018 12:44 PM.
- */
 public class MainActivity extends AppCompatActivity {
 
     @Override
