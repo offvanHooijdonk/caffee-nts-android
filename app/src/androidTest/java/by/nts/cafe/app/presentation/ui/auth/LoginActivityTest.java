cab3cd6a12d18992052a6a9fc746a8f7ef3db574
Yahor Fralou <yahor.fralou@datalex.com>
2018-05-14 18:46:20 +0300

    Added sample test. Added Kotlin support.

---
+++ app/src/androidTest/java/by/nts/cafe/app/presentation/ui/auth/LoginActivityTest.java
@@ -0,0 +1,23 @@
+package by.nts.cafe.app.presentation.ui.auth;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import junit.framework.Assert;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> rule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void onUserClicked() {
+        int itemsCount = rule.getActivity().rvUsers.getAdapter().getItemCount();
+        Assert.assertTrue("Expected users > 0, found: " + itemsCount, itemsCount > 0);
+    }
+}
